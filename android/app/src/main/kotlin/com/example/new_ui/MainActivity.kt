b85f842b4d7491cf95475dd87df9125c915e4fc4
Vian Shah <124880161+vianshah3004@users.noreply.github.com>
2025-06-04 18:54:38 +0530

    Delete android directory

--- android/app/src/main/kotlin/com/example/new_ui/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.firebase_demo  // Change to your actual package name
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity() {
-    // No additional code needed
-}
